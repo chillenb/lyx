b557fa4c73c3c27ccbb3a80d366f32ccf4d86a3f
Juergen Spitzmueller <spitz@lyx.org>
2026-02-22 14:18:32 +0100

    Add support for Sioyek PDF reader
    and document its synctex settings

--- lib/configure.py
+++ lib/configure.py
@@ -887,7 +887,7 @@ def checkFormatEntries(dtl_tools):
     checkViewer('a PDF previewer',
                 ['pdfview', 'kpdf', 'okular', 'qpdfview --unique',
                  'evince', 'papers', 'xreader', 'kghostview', 'xpdf', 'SumatraPDF',
-                 'acrobat', 'acroread', 'mupdf', 'Skim.app',
+                 'acrobat', 'acroread', 'mupdf', 'sioyek', 'Skim.app',
                  'gv', 'ghostview', 'AcroRd32', 'gsview64', 'gsview32'],
         rc_entry = [r'''\Format pdf        pdf    "PDF (ps2pdf)"          P  "%%"	""	"document,vector,menu=export"	""
 \Format pdf2       pdf    "PDF (pdflatex)"        F  "%%"	""	"document,vector,menu=export"	""
