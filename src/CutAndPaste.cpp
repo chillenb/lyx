a2e701af72513a7b4dcf5c4e8d5f63523dbf5904
Juergen Spitzmueller <spitz@lyx.org>
2026-04-08 19:12:13 +0200

    Fix pasting from LaTeX with keepEmpty environment in first paragraph (#13295)

--- src/CutAndPaste.cpp
+++ src/CutAndPaste.cpp
@@ -1357,7 +1357,7 @@ bool pasteClipboardText(Cursor & cur, ErrorList & errorList, bool asParagraphs,
 				available = buffer.importString(names[i], text, errorList);
 				if (available)
 					available = !buffer.paragraphs().empty();
-				if (available && !buffer.paragraphs()[0].empty()) {
+				if (available && (!buffer.paragraphs()[0].empty() || buffer.paragraphs()[0].layout().keepempty)) {
 					// TeX2lyx (also used in the HTML chain) assumes English as document language
 					// if no language is explicitly set (as is the case here).
 					// We thus reset the temp buffer's language to the context language
